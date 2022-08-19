b7ea9ca051b49b57c970cc40a8a22ee041248e9a
yuzu-eva <stevenhu@web.de>
2022-08-19 03:07:58 +0200

    added fullgaps

--- config.h
+++ config.h
@@ -6,6 +6,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int gappx              = 5;
 static const char *fonts[]          = { "Hack Nerd Font:size=11", "Symbola:pixelsize=11:antialias=true:autohint=true" };
 static const char dmenufont[]       = "Hack Nerd Font:size=11";
 static const char col_gray1[]       = "#222222";
