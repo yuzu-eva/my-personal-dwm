a8f3b8b0dc08ecc4033b8470c34a8cb067f56286
yuzu-eva <stevenhu@web.de>
2022-08-19 22:04:01 +0200

    uncommented the iscol (only way to prevent crashes for now)

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel		= 0;
 static const int gappx              = 5;
-static const char *fonts[]          = { "Hack Nerd Font:size=11", "Symbola:pixelsize=11:antialias=true:autohint=true" };
+static const char *fonts[]          = { "Hack Nerd Font:size=11"};
 static const char dmenufont[]       = "Hack Nerd Font:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
