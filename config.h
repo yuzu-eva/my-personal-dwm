03399cc7b724406fa144cc0c89aa54ea17f2e3fe
yuzu-eva <stevenhu@web.de>
2022-08-31 20:57:21 +0200

    changed volume control cmd to change current sink

--- config.h
+++ config.h
@@ -72,9 +72,9 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *roficmd[] = { "rofi", "-show", "drun", "-show-icons", NULL };
 static const char *termcmd[]  = { "kitty", NULL };
-static const char *upvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%", NULL };
-static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%", NULL };
-static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute", "0", "toggle", NULL };
+static const char *upvol[] = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL };
+static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%", NULL };
+static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute", "@DEFAULT_SINK@", "toggle", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -120,7 +120,7 @@ static Key keys[] = {
 
 	{ 0,    XF86XK_AudioRaiseVolume,    spawn, {.v = upvol} },
 	{ 0,	XF86XK_AudioLowerVolume,    spawn, {.v = downvol} },
-	{ 0,	XF86XK_AudioMute,            spawn, {.v = mutevol} },
+	{ 0,	XF86XK_AudioMute,           spawn, {.v = mutevol} },
 
 };
 
