4adbbf8b563aac825e3f5ca341ac30a1fbda50d9
yuzu-eva <stevenhu@web.de>
2022-11-17 22:48:31 +0100

    disabled resizehints

--- config.h
+++ config.h
@@ -33,17 +33,18 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
- 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
- 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
- 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
- 	{ "kitty",   NULL,     NULL,           0,         0,          1,           0,        -1 },
- 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+ 	/* class           instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+ 	{ "Gimp",          NULL,     NULL,           0,         1,          0,           0,        -1 },
+ 	{ "Firefox",       NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+ 	{ "kitty",         NULL,     NULL,           0,         0,          1,           0,        -1 },
+ 	{ "st-256color",   NULL,     NULL,           0,         0,          1,           0,        -1 },
+ 	{ NULL,            NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 #include "vanitygaps.c"
