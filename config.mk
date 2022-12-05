9f9f86ce27f1f5d739a44e7628136bf6bda070e8
yuzu-eva <stevenhu@web.de>
2022-12-06 00:21:04 +0100

    lib changes

--- config.mk
+++ config.mk
@@ -7,10 +7,10 @@ VERSION = 6.3
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
-# X11INC = /usr/X11R6/include
-# X11LIB = /usr/X11R6/lib
-X11INC = /home/cafebabe/.local/src/libxft/include
-X11LIB = /home/cafebabe/.local/src/libxft/src/.libs
+X11INC = /usr/X11R6/include
+X11LIB = /usr/X11R6/lib
+# X11INC = /home/$USER/.local/src/libxft/include
+# X11LIB = /home/$USER/.local/src/libxft/src/.libs
 
 # Xinerama, comment if you don't want it
 XINERAMALIBS  = -lXinerama
@@ -32,8 +32,8 @@ LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS} -lX11-xcb -lxcb -lxcb-r
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
 #CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
 CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
-# LDFLAGS  = ${LIBS}
-LDFLAGS = -Xlinker -rpath=$(X11LIB) $(LIBS)
+LDFLAGS  = ${LIBS}
+# LDFLAGS = -Xlinker -rpath=$(X11LIB) $(LIBS)
 
 # Solaris
 #CFLAGS = -fast ${INCS} -DVERSION=\"${VERSION}\"
